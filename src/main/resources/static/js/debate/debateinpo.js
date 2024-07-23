475f953e4c0bfdd8532b93a6baf22ac7c80bf0b4
leejaejuong <user.email>
2024-07-24 00:20:14 +0900

    작업내용 업데이트 detateinfo css 추가

--- src/main/resources/static/js/debate/debateinpo.js
+++ src/main/resources/static/js/debate/debateinpo.js
@@ -1,8 +1,29 @@
+// {
+//     let $nextmake = document.querySelector('.gather-create-butten-before');
+//
+//     $nextmake.addEventListener('click',function (){
+//         location.href=`/member/debatemake`;
+//     })
+// }
+
+
+
+
+
+
 {
-    let $nextBox = document.querySelector('.gather-create-butten-div');
+    let $slider = document.querySelector('.gather-group');
+    let $output = document.querySelector('.gather-value');
+$output.innerHTML=$slider.value;
+$slider.oninput=function (){
+    $output.innerHTML=this.value;
+}
+    let $sliderD = document.querySelector('.gather-group-d');
+    let $outputD = document.querySelector('.gather-value-d');
+    $outputD.innerHTML=$sliderD.value;
+    $sliderD.oninput=function (){
+        $outputD.innerHTML=this.value;
+    }
 
 
-    $nextBox.addEventListener('click', function () {
-        location.href = `/member/debatetype`;
-    });
 }
